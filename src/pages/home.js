6bdc6e1e56ec6edc2c1bc95f677e5d8108b21d7e
Nthuyduong <nguyenthuyduong15113@gmail.com>
2023-05-20 13:49:20 +0700

    homepage top trending

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,18 +4,57 @@ const Home = () => {
     return (
         <div>
             <div className="container-fluid">
-                {/*TOP TRENDING*/}
                 <div className="">
+                    {/*TOP TRENDING*/}
+                    <div className="top-trend row">
+                        <div className="col-3 trend-content">
+                            <div className="content-inner">
+                                <div className="medium_text">Top 3 Hottest Topic</div>
+                                <div className="heading_4 my-3">Trending Recipes</div>
+                                <div>One of the great aspects of social media is spotting trends faster than ever before.
+                                    If you like to be ahead of the curve when it comes to cake, check out some of the
+                                    sweetest</div>
+                            </div>
+                        </div>
+                        <div className="col-4 trend-start">
+                            <div className="trend-section">
+                                <img src="./image/home/trend1.png"/>
+                                <div className="trend-title row">
+                                    <div className="heading_2">01/</div>
+                                    <div className="trend-text">
+                                        <div className="medium_text">Dessert</div>
+                                        <div className="heading_6">CHOCOLATE CAKE</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-3 trend-center">
+                            <div className="trend-section">
+                                <img src="./image/home/trend2.jpg"/>
+                                <div className="trend-title">
+                                    <div className="heading_2">02/</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-2 trend-end">
+                            <div className="trend-section">
+                                <img src="./image/home/trend3.jpg"/>
+                                <div className="trend-title">
+                                    <div className="heading_2">03/</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     {/*RECENTLY UPDATE*/}
                     <div className="recently-update">
-                        <div className="title row">
-                            <div className="col-2"></div>
-                            <div className="col-8 text-center">
-                                <h4>Recently Update</h4>
-                                <div className="line"></div>
-                                <p>Cake enthusiast? Checked our blog recently? Go on now.</p>
+                        <div className="title row mb-5">
+                            <div className="col-3"></div>
+                            <div className="col-6 text-center">
+                                <div className="heading_3 mb-3">Recently Update</div>
+
+                                <div>Cake enthusiast? Checked our blog recently? Go on now.</div>
                             </div>
-                            <div className="col-2"></div>
+                            <div className="col-3"></div>
                         </div>
                         <div className="update">
                             <div className="row">
@@ -96,6 +135,14 @@ const Home = () => {
                     {/*</div>*/}
                     {/*FOR THIS SEASON*/}
                     <div className="for-season">
+                        <div className="title row mb-5">
+                            <div className="col-3"></div>
+                            <div className="col-6 text-center">
+                                <div className="heading_3 mb-3">For This Season</div>
+                                <div>Cake enthusiast? Checked our blog recently? Go on now.</div>
+                            </div>
+                            <div className="col-3"></div>
+                        </div>
                         <div className="row">
                             <div className="col-3">
                                 <div className="update-cart">
