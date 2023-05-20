552e672e5cb0621078d92f508d17afd4a1206e2d
Nthuyduong <nguyenthuyduong15113@gmail.com>
2023-05-20 23:34:05 +0700

    commit

--- src/pages/recipedetail.js
+++ src/pages/recipedetail.js
@@ -2,7 +2,45 @@ import React from "react";
 
 const Recipedetail = () => {
     return (
-        <div>This is recipe detail page</div>
+        <div>
+            <div className="container-fluid">
+                {/*PAGINATION*/}
+                <div></div>
+                {/*RECIPE DETAIL HERE*/}
+                <div className="row">
+                    {/*ARTICLE CONTENT HERE*/}
+                    <div className="col-8">
+                        <div className="row">
+                            <div className="col-10">
+                                <div>Published Aug 19, 2022</div>
+                                <div className="heading_3">Chocolate Raspberry Cream Cake</div>
+                            </div>
+                            <div className="col-2">
+                                <div className="d-flex">
+                                    <div>Prep</div>
+                                    <div>15 Mins</div>
+                                </div>
+                                <div className="d-flex">
+                                    <div>Prep</div>
+                                    <div>15 Mins</div>
+                                </div>
+                                <div className="d-flex">
+                                    <div>Prep</div>
+                                    <div>15 Mins</div>
+                                </div>
+                                <div className="d-flex">
+                                    <div>Prep</div>
+                                    <div>15 Mins</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col"></div>
+                    {/*SIDE BAR HERE*/}
+                    <div className="col-3"></div>
+                </div>
+            </div>
+        </div>
     )
 }
 
